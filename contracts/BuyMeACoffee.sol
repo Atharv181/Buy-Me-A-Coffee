c522f0f9a6b62731f16563585b7f7bb7a683f267
Atharv181 <atharvbobade181@gmail.com>
2022-09-14 18:09:56 +0530

    Added smart contract and script

--- contracts/BuyMeACoffee.sol
+++ contracts/BuyMeACoffee.sol
@@ -3,7 +3,58 @@ pragma solidity ^0.8.4;
 
 
 contract BuyMeACoffee {
-    
+    // Event to emit when a Memo is created.
+    event NewMemo(
+        address from ,
+        uint256 timestamp ,
+        string name ,
+        string message
+    );
 
-    
+    //Memo Struct
+    struct Memo{
+        address from ;
+        uint256 timestamp ;
+        string name ;
+        string message;
+    }
+
+    // List of all Memos received from all
+    Memo[] memos;
+
+    // Address of contract deployer
+    address payable owner;
+
+    constructor(){
+        owner = payable(msg.sender);
+    }
+
+    function buyCoffe(string memory _name, string memory _message) payable public {
+        require(msg.value > 0 ,"Can't buy coffee with 0 ETH");
+        // Add the memo to storage 
+        memos.push(Memo(
+            msg.sender,
+            block.timestamp,
+            _name,
+            _message
+        ));
+
+        // Emit a log evevnt when a new mwmo is created!
+        emit NewMemo(
+            msg.sender,
+            block.timestamp,
+            _name,
+            _message
+        );
+    }
+
+    // Send the entire balance present in the contract to owner
+    function withdrawTips() public {
+        address(this).balance;
+        require(owner.send(address(this).balance));
+    }
+
+    function getMemos() public view returns(Memo[] memory) {
+        return memos;
+    }
 }
